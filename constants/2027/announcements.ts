b2b4f737bffdc9032fa3846f98b7a9bf548a7d88
Aryan Ghosh <aryan.ghosh.tentue@gmail.com>
2026-08-03 18:29:10 +0530

    new announcement

--- constants/2027/announcements.ts
+++ constants/2027/announcements.ts
@@ -6,8 +6,8 @@ export type Announcement = {
 
 export const ANNOUNCEMENTS: Announcement[] = [
   {
-    header: "ICAA 27 Paper Submission Portal Opens",
-    date: "Aug 01, 2026",
+    header: "ICAA 27 Paper Submission Portal is Live",
+    date: "Aug 01, 2026 - Sept 01, 2026",
     desc: "Papers presenting original contributions related to the design, analysis, and implementation of algorithms are sought.",
   },
   {
